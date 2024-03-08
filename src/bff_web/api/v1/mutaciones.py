e266b574fdc3cd5ddc42381497fb22a17c0a7f14
Nestor  Pérez <n.pereze@uniandes.edu.co>
2024-03-08 03:38:45 +0000

    Ajustar despachador mutaciones

--- src/bff_web/api/v1/mutaciones.py
+++ src/bff_web/api/v1/mutaciones.py
@@ -17,29 +17,15 @@ class Mutation:
     @strawberry.mutation
     async def registrar_propiedad(self, id_propiedad: str, nombre_propiedad: str, estado_propiedad: str, cliente_propiedad: str, info: Info) -> PropiedadRespuesta:
         print(f"ID Propiedad: {id_propiedad}, Tipo Transaccion: {nombre_propiedad}")
-        propiedadLoad = dict(
+        propiedad = Propiedad(
             id_propiedad = id_propiedad,
             nombre_propiedad = nombre_propiedad,
             estado_propiedad = estado_propiedad,
             cliente_propiedad = cliente_propiedad
         )
-
-        comando = dict(
-            id = str(uuid.uuid4()),
-            time=utils.time_millis(),
-            #specversion = "v1",
-            type = "ComandoCiente",
-            ingestion=utils.time_millis(),
-            datacontenttype="AVRO",
-            service_name = "BFF Web",
-            data = propiedadLoad
-        ) 
-
-   
-        print(f" ComandoIntegra*******: {comando}")
+  
         despachador = Despachador()
-        #info.context["background_tasks"].add_task(despachador.publicar_mensaje, comando, "comandos-cliente", "public/default/comando-cliente")
-        info.context["background_tasks"].add_task(despachador.publicar_comando, comando, "comandos-cliente")
+        info.context["background_tasks"].add_task(despachador.publicar_comando, propiedad, "comandos-cliente")
         
         return PropiedadRespuesta(mensaje="Procesando Mensaje", codigo=203)
 
--- src/bff_web/despachadores.py
+++ src/bff_web/despachadores.py
@@ -9,20 +9,6 @@ class Despachador:
     def __init__(self):
         ...
 
-
-    """ async def publicar_evento(self, evento, topico):
-        evento_integracion = EventoTransaccionCreada(data=evento)
-        self._publicar_mensaje(evento_integracion, topico, AvroSchema(EventoTransaccionCreada))
-
-
-
-    def _publicar_mensaje(self, mensaje, topico, schema):
-        cliente = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
-        publicador = cliente.create_producer(topico, schema=schema)
-        publicador.send(mensaje)
-        print(f"===================== Mensaje publicado en [{topico}] =====================")
-        cliente.close() """
-
     def _publicar_mensaje(self, mensaje, topico, schema):
         cliente = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
         publicador = cliente.create_producer(topico, schema=AvroSchema(ComandoCrearPropiedad))
@@ -32,24 +18,16 @@ class Despachador:
 
 
     def publicar_comando(self, comando, topico):
-        """ payload = ComandoCreaPropiedadPayload(
+        payload = ComandoCreaPropiedadPayload(
             id_propiedad = str(comando.id_propiedad),
             nombre_propiedad = str(comando.nombre_propiedad),
             estado_propiedad = str(comando.estado_propiedad),
             cliente_propiedad = str(comando.cliente_propiedad)
         )
-        comando_integracion = ComandoCrearPropiedad(data=payload) """
-        self._publicar_mensaje(comando, topico, AvroSchema(ComandoCrearPropiedad))
+        comando_integracion = ComandoCrearPropiedad(data=payload)
+        self._publicar_mensaje(comando_integracion, topico, AvroSchema(ComandoCrearPropiedad))
         print(f"===================== Comando publicado en [{topico}] =====================")
 
-    async def publicar_mensaje(self, mensaje, topico, schema):
-        json_schema = utils.consultar_schema_registry(schema)  
-        avro_schema = utils.obtener_schema_avro_de_diccionario(json_schema)
-
-        cliente = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
-        publicador = cliente.create_producer(topico, schema=avro_schema)
-        publicador.send(mensaje)
-        cliente.close()
 
 
  
